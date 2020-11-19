792362ae1b33f292a2d4c1fdbebad2b01f05357d
Maohan Sun <lampoon2gn@gmail.com>
2020-11-19 12:18:41 -0700

    + movie table with fluent api

--- MovieShop.Core/Entities/Genre.cs
+++ MovieShop.Core/Entities/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -9,6 +10,8 @@ namespace MovieShop.Core.Entities
     public class Genre
     {
         public int Id { get; set; }
+
+        [MaxLength(24)]
         public string Name { get; set; }
     }
 }
--- MovieShop.Infrastructure/Data/MovieShopDbContext.cs
+++ MovieShop.Infrastructure/Data/MovieShopDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using MovieShop.Core.Entities;
 
 namespace MovieShop.Infrastructure.Data
@@ -12,6 +13,31 @@ namespace MovieShop.Infrastructure.Data
         public MovieShopDbContext(DbContextOptions<MovieShopDbContext> options) : base(options)
         {
         }
+
+        //override this to use fluent api
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //action delegate takes a method with void return type
+            modelBuilder.Entity<Movie>(ConfigureMovie);
+        }
+
+        private void ConfigureMovie(EntityTypeBuilder<Movie> builder)
+        {
+            //place to confugure our movie entity
+            builder.ToTable("Movie");
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Title).IsRequired().HasMaxLength(256);
+            builder.Property(m => m.Overview).HasMaxLength(4096);
+            builder.Property(m => m.Tagline).HasMaxLength(512);
+            builder.Property(m => m.ImdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.TmdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.PosterUrl).HasMaxLength(2084);
+            builder.Property(m => m.BackdropUrl).HasMaxLength(2084);
+            builder.Property(m => m.OriginalLanguage).HasMaxLength(64);
+            builder.Property(m => m.Price).HasColumnType("decimal(5, 2)").HasDefaultValue(9.9m);
+            builder.Property(m => m.CreatedDate).HasDefaultValueSql("getdate()");
+        }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
     }
 }
