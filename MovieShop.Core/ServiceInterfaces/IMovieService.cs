b56d60a08a46ed3a2ec1244b8ab9b6fb9557a401
Maohan Sun <lampoon2gn@gmail.com>
2020-11-29 11:38:29 -0700

    + IUserService, some request/response models

--- MovieShop.Core/ServiceInterfaces/IMovieService.cs
+++ MovieShop.Core/ServiceInterfaces/IMovieService.cs
@@ -1,4 +1,5 @@
-﻿using MovieShop.Core.Models.Response;
+﻿using MovieShop.Core.Models.Request;
+using MovieShop.Core.Models.Response;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,12 +13,14 @@ namespace MovieShop.Core.ServiceInterfaces
         //Task<PagedResultSet<MovieResponseModel>> GetAllMoviePurchasesByPagination(int pageSize = 20, int page = 0);
         //Task<PaginatedList<MovieResponseModel>> GetAllPurchasesByMovieId(int movieId);
         Task<MovieDetailsResponseModel> GetMovieAsync(int id);
-        //Task<IEnumerable<ReviewMovieResponseModel>> GetReviewsForMovie(int id);
+        Task<IEnumerable<ReviewMovieResponseModel>> GetReviewsForMovie(int id);
         Task<int> GetMoviesCount(string title = "");
         Task<IEnumerable<MovieResponseModel>> GetTopRatedMovies();
         Task<IEnumerable<MovieResponseModel>> GetTopRevenueMovies();
         Task<IEnumerable<MovieResponseModel>> GetMoviesByGenre(int genreId);
-        //Task<MovieDetailsResponseModel> CreateMovie(MovieCreateRequest movieCreateRequest);
-        //Task<MovieDetailsResponseModel> UpdateMovie(MovieCreateRequest movieCreateRequest);
+        Task<MovieDetailsResponseModel> CreateMovie(MovieCreateRequest movieCreateRequest);
+        Task<MovieDetailsResponseModel> UpdateMovie(MovieCreateRequest movieCreateRequest);
+
+
     }
 }
